473c67d1faecd0a13b672f701a5b21706800de83
GoncaloDomingues750 <up202007914@up.pt>
2022-03-29 14:32:24 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/DA_project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DA_project.dir/main.cpp.obj"
+  "DA_project.exe"
+  "DA_project.exe.manifest"
+  "DA_project.pdb"
+  "libDA_project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DA_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
