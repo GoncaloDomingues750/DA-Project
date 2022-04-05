8a2cba498ed7a37265a8b14b36ebfd8e97bec32a
GoncaloDomingues750 <up202007914@up.pt>
2022-04-05 15:52:59 +0100

    New tests

--- Tests/PackageTests.cpp
+++ Tests/PackageTests.cpp
@@ -19,3 +19,20 @@ TEST(test1, getWeight){
     EXPECT_EQ(10, p1.getWeight());
     EXPECT_EQ(5, p2.getWeight());
 }
+
+TEST(test1, getVoloume){
+    Package p1(0, 10, 5, true, 2);
+    Package p2(2, 5, 10, false, 50);
+
+    EXPECT_EQ(5, p1.getVolume());
+    EXPECT_EQ(10, p2.getVolume());
+}
+
+TEST(test1, getPriority){
+    Package p1(0, 10, 5, true, 2);
+    Package p2(2, 5, 10, false, 50);
+
+    EXPECT_EQ(true, p1.getPriority());
+    EXPECT_EQ(false, p2.getPriority());
+}
+
