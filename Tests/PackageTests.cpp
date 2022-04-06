87e92667b1e92ea88d74d12fe66526ab586a27b9
GoncaloDomingues750 <up202007914@up.pt>
2022-04-06 15:22:20 +0100

    More tests

--- Tests/PackageTests.cpp
+++ Tests/PackageTests.cpp
@@ -36,3 +36,39 @@ TEST(test1, getPriority){
     EXPECT_EQ(false, p2.getPriority());
 }
 
+TEST(test1, getDuration){
+    Package p1(0, 10, 5, true, 2);
+    Package p2(2, 5, 10, false, 50);
+
+    EXPECT_EQ(2, p1.getDuration());
+    EXPECT_EQ(50, p2.getDuration());
+}
+
+TEST(test1, setPackageNumber){
+    Package p1(0, 10, 5, true, 2);
+    Package p2(2, 5, 10, false, 50);
+
+    EXPECT_EQ(0, p1.getPackageNumber());
+    EXPECT_EQ(2, p2.getPackageNumber());
+
+    p1.setPackageNumber(50);
+    p2.setPackageNumber(20);
+
+    EXPECT_EQ(50, p1.getPackageNumber());
+    EXPECT_EQ(20, p2.getPackageNumber());
+}
+
+TEST(test1, setWeight){
+    Package p1(0, 10, 5, true, 2);
+    Package p2(2, 5, 10, false, 50);
+
+    EXPECT_EQ(10, p1.getWeight());
+    EXPECT_EQ(5, p2.getWeight());
+
+    p1.setWeight(50);
+    p2.setWeight(20);
+
+    EXPECT_EQ(50, p1.getWeight());
+    EXPECT_EQ(20, p2.getWeight());
+}
+
