26f3a7c86b07615281926dc3adf99f8a906f380e
GoncaloDomingues750 <93599643+GoncaloDomingues750@users.noreply.github.com>
2022-04-22 19:24:25 +0100

    Delete cmake-build-debug directory

--- cmake-build-debug/lib/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/aed/DA-project/lib/googletest-master/googletest
-# Build directory: C:/aed/DA-project/cmake-build-debug/lib/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
